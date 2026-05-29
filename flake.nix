2081543370373ce4049f42eb09033251448f244f
gleb <116607327+loeclos@users.noreply.github.com>
2026-05-28 22:05:49 -0700

    change stuff

--- flake.nix
+++ flake.nix
@@ -1,30 +1,87 @@
 {
-  description = "Helium browser flake";
+  description = "Helium AppImage nixified";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
 
-  outputs =
-    { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-    in
-    {
-      packages.${system}.helium = pkgs.callPackage ./pkgs/helium.nix { };
-
-      packages.${system}.default = self.packages.${system}.helium;
-
-      apps.${system}.helium = {
-        type = "app";
-        program = "${self.packages.${system}.helium}/bin/helium";
-      };
-
-      apps.${system}.default = self.apps.${system}.helium;
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
     };
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        helium =
+          {
+            commandLineArgs ? [ ],
+            enableFeatures ? [ ],
+            libvaSupport ? pkgs.stdenv.hostPlatform.isLinux,
+          }:
+          pkgs.appimageTools.wrapType2 rec {
+
+            pname = "helium";
+            version = "0.12.4.1";
+
+            src = pkgs.fetchurl {
+              url = "https://github.com/imputnet/helium-linux/releases/download/${version}/${pname}-${version}-x86_64.AppImage";
+              sha256 = "sha256-OgS8HkLBseFrEhNFJxMwp1bg0gzPdfY1VaySAAp7vq0=";
+            };
+
+            _enableFeatures =
+              enableFeatures
+              ++ pkgs.lib.optionals libvaSupport [
+                "VaapiVideoDecoder"
+              ];
+
+            extraPkgs = pkgs: pkgs.lib.optionals libvaSupport [ pkgs.libva ];
+
+            extraBwrapArgs = [
+              "--ro-bind-try /etc/chromium /etc/chromium"
+            ];
+
+            nativeBuildInputs = [ pkgs.makeWrapper ];
+
+            extraInstallCommands =
+              let
+                contents = pkgs.appimageTools.extract { inherit pname version src; };
+              in
+              ''
+                wrapProgram $out/bin/${pname} \
+                  ${
+                    pkgs.lib.optionalString (
+                      _enableFeatures != [ ]
+                    ) "--add-flags \"--enable-features=${pkgs.lib.strings.concatStringsSep "," _enableFeatures}\""
+                  } \
+                  ${pkgs.lib.optionalString (
+                    commandLineArgs != [ ]
+                  ) "--add-flags \"${pkgs.lib.strings.concatStringsSep " " commandLineArgs}\""}
+                install -m 444 -D ${contents}/${pname}.desktop -t $out/share/applications
+                substituteInPlace $out/share/applications/${pname}.desktop \
+                  --replace 'Exec=AppRun' 'Exec=${pname}'
+                cp -r ${contents}/usr/share/icons $out/share
+              '';
+
+          };
+      in
+      with pkgs;
+      {
+        inherit helium;
+        devShells.default = mkShell {
+          buildInputs = [ statix ];
+        };
+        defaultPackage = helium { };
+      }
+    );
 }
